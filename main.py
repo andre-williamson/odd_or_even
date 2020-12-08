7c8a56d6c02d4700a834dc66b52fbdeeb3ec90ac
andre williamson <AndreWilliamsonMN@gmail.com>
2020-12-08 04:48:19 +0000

    made program that tell yoou if it's even of odd

--- main.py
+++ main.py
@@ -3,7 +3,10 @@ number = int(input("Which number do you want to check? "))
 # 🚨 Don't change the code above 👆
 
 #Write your code below this line 👇
-
+if number % 2 ==0:
+ print("it's even bro")
+else:
+ print("Its odd bro")  
 
 
 
